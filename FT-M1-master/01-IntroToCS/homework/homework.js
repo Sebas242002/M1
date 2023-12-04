8263628e92e2630e15cfc81ec52f89a61a4377e4
Sebas242002 <sebas2002hg@gmail.com>
2023-12-04 12:33:27 -0500

    Clase1

--- FT-M1-master/01-IntroToCS/homework/homework.js
+++ FT-M1-master/01-IntroToCS/homework/homework.js
@@ -1,21 +1,8 @@
 'use strict';
 
 function BinarioADecimal(num) {
-   let decimal = 0
-   for (let i = 0; i < num.length; i++) {
-      const bit = Number(num.charAt(i));
-      const posicion = num.length -i -1;
-      decimal += bit * Math.pow(2, posicion);
-   } 
-   return decimal;
 }
 function DecimalABinario(num) {
-   let binario = '';
-   while (num > 0) {
-      binario = (num % 2) + binario;
-      num = Math.floor(num / 2);
-   }
-   return binario;
 }
 
 module.exports = {
